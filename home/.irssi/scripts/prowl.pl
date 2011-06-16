4f54cdd9ac1364f0f2dd2815b917f9e9b0e370f9
Mark Percival <mark@markpercival.us>
2011-06-16 06:31:55 +0000

    Irssi

--- home/.irssi/scripts/prowl.pl
+++ home/.irssi/scripts/prowl.pl
@@ -1,142 +1,98 @@
+#   Copyright 2011 Josh Kearney
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 use strict;
 use warnings;
 
-#####################################################################
-# This script sends notifications to your
-# iPhone using the prowl API when you are away.
-#
-# This script is loosely based on Denis Lemire's
-# script, you can find his page on: http://www.denis.lemire.name/
-#
-# This script have some major differences in both code and usage
-# from his script. The configuration he uses *won't* work with
-# this script.
-#
-# Commands:
-# /set prowl_api_key API_KEY
-# /set prowl_general_hilight on/off
-# /set prowl_priority_channel -2 up to 2
-# /set prowl_priority_pm -2 up to 2
-# /set prowl_priority_general -2 up to 2
-#
-# "General hilight" basically referrs to ALL the hilights you have
-# added manually in irssi, if many, it can get really bloated if
-# turned on. Default is Off.
-#
-# You will need the following packages:
-# LWP::UserAgent (You can install this using cpan -i LWP::UserAgent)
-# Crypt::SSLeay  (You can install this using cpan -i Crypt::SSLeay)
-# 
-# Or if you're using Debian GNU/Linux:
-# apt-get update;apt-get install libwww-perl libcrypt-ssleay-perl
-#
-#
-# eth0 will prevail. || irc.eth0.info
-#
-#####################################################################
-
-
 use Irssi;
-use Irssi::Irc;
+use LWP::Simple;
 use vars qw($VERSION %IRSSI);
 
-use LWP::UserAgent;
-
-$VERSION = "0.4";
+$VERSION = "0.0.1";
 
 %IRSSI = (
-    authors     => "Caesar 'sniker' Ahlenhed",
-    contact     => "sniker\@se.linux.org",
-    name        => "prowl",
-    description => "Sends prowlnotifcations when away",
-    license     => "GPLv2",
-    url         => "http://sniker.codebase.nu",
-    changed     => "Mon Mar 15 18:53:32 CET 2010",
+    authors => "Josh Kearney",
+    contact => "josh\@jk0.org",
+    name => "irssi-prowl",
+    description => "Send Prowl notifications upon new mentions or PMs when away.",
+    license => "Apache License, Version 2.0",
+    url => "https://github.com/jk0/irssi-prowl"
 );
 
-# Configuration settings and default values.
-Irssi::settings_add_str("prowl", "prowl_api_key", "");
-Irssi::settings_add_bool("prowl", "prowl_general_hilight", 0);
-Irssi::settings_add_str("prowl", "prowl_priority_channel", 0);
-Irssi::settings_add_str("prowl", "prowl_priority_pm", 0);
-Irssi::settings_add_str("prowl", "prowl_priority_general", 0);
+sub is_away {
+    my ($server) = @_;
 
-# The whole "send_noti" function is pretty much taken from the prowl example script.
-sub send_noti {
-    my ($event, $text, $priority) = @_;
+    return $server->{usermode_away} ? 1 : 0;
+}
 
-    my %options = ();
+sub msg_public {
+    my ($server, $data, $nick, $mask, $target) = @_;
 
-    $options{'application'} ||= "Irssi";
-    $options{'event'} = $event;
-    $options{'notification'} = $text;
-    $options{'priority'} = $priority;
-    
-
-    # This check isn't really needed, just added if someone would
-    # would define $options{'apikey'} manually in the script.
-    if(!exists($options{'apikey'})){
-        $options{'apikey'} = Irssi::settings_get_str("prowl_api_key");
-        chomp $options{'apikey'};
-    }else{
-        Irssi::print("There is some kind of trouble with the API key.");
-    }
-
-    $options{'application'} =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-    $options{'event'} =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-    $options{'notification'} =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
-
-    my ($userAgent, $request, $response, $requestURL);
-    $userAgent = LWP::UserAgent->new;
-    $userAgent->agent("ProwlScript/1.0");
-
-    $requestURL = sprintf("https://prowl.weks.net/publicapi/add?apikey=%s&application=%s&event=%s&description=%s&priority=%d",
-        $options{'apikey'},
-        $options{'application'},
-        $options{'event'},
-        $options{'notification'},
-        $options{'priority'});
-
-    $request = HTTP::Request->new(GET => $requestURL);
-    $response = $userAgent->request($request);
-
-    if ($response->is_success) {
-        # I guess it worked, eh?
-    } elsif ($response->code == 401) {
-        Irssi::print( "Notification not posted: incorrect API key.");
-    } else {
-        Irssi::print("Notification not posted: " . $response->content);
+    if(is_away($server) && ($data =~ /$server->{nick}/i)) {
+        send_notification($server->{chatnet}, "[$target:$nick] $data");
     }
 }
 
-sub pubmsg {
-    my ($server, $data, $nick) = @_;
+sub msg_private {
+    my ($server, $text, $nick, $host, $channel) = @_;
 
-    if($server->{usermode_away} == 1 && $data =~ /$server->{nick}/i){
-        send_noti("Hilighted", $nick . ': ' . $data, Irssi::settings_get_str("prowl_priority_channel"));
+    if(is_away($server)) {
+        send_notification($server->{chatnet}, "[$nick] $text");
     }
 }
 
-sub privmsg {
-    my ($server, $data, $nick) = @_;
-    if($server->{usermode_away} == 1){
-        send_noti("PM", $nick . ': ' . $data, Irssi::settings_get_str("prowl_priority_pm"));
-    }
+sub send_notification {
+    my ($chatnet, $notice) = @_;
+    my $enabled = Irssi::settings_get_bool("prowl_enabled");
+    my $api_key = Irssi::settings_get_str("prowl_api_key");
+
+    return unless($enabled && (length($api_key) > 0));
+
+    $notice =~ s/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg;
+
+    my $url = sprintf("http://prowlapp.com/publicapi/add?apikey=%s&"
+                      . "application=Irssi&event=%s&description=%s",
+                      $api_key, $chatnet, $notice);
+    get($url);
 }
 
-sub genhilight {
-    my($dest, $text, $stripped) = @_;
-    my $server = $dest->{server};
+sub proxy_connect {
+    my (@servers) = Irssi::servers();
 
-    if($dest->{level} & MSGLEVEL_HILIGHT) {
-        if($server->{usermode_away} == 1){
-            if(Irssi::settings_get_bool("prowl_general_hilight")){
-                send_noti("General Hilight", $stripped, Irssi::settings_get_str("prowl_priority_general"));
-            }
+    foreach my $server (@servers) {
+        if(is_away($server)) {
+            $server->command("AWAY -one");
         }
     }
 }
 
-Irssi::signal_add_last('message public', 'pubmsg');
-Irssi::signal_add_last('message private', 'privmsg');
-Irssi::signal_add_last('print text', 'genhilight');
+sub proxy_disconnect {
+    my (@servers) = Irssi::servers();
+    my $away_msg = Irssi::settings_get_str("prowl_away_msg");
+
+    foreach my $server (@servers) {
+        if(!is_away($server)) {
+            $server->command("AWAY -one $away_msg");
+        }
+    }
+}
+
+Irssi::settings_add_str("prowl", "prowl_api_key", "");
+Irssi::settings_add_str("prowl", "prowl_away_msg", "afk");
+Irssi::settings_add_bool("prowl", "prowl_enabled", 0);
+
+Irssi::signal_add_last("message public", "msg_public");
+Irssi::signal_add_last("message private", "msg_private");
+Irssi::signal_add_last("proxy client connected", "proxy_connect");
+Irssi::signal_add_last("proxy client disconnected", "proxy_disconnect");
