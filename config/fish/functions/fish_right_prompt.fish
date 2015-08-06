e23558ad13fc5149ce30f83de68c7c549be559fa
Ben Radler <benradler@me.com>
2015-08-06 01:16:22 -0700

    Update right prompt to show node version

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
 # Right-hand prompt
 function fish_right_prompt
   set -l rb_version (command ruby -e 'print RUBY_VERSION' ^/dev/null)
-  echo -s (set_color blue) "(💎  $rb_version)" (set_color green) (paradox_command_duration) (set_color normal)
+  set -l node_version (command node --version ^/dev/null)
+  echo -s (set_color blue) "(💎  $rb_version  ☕️  $node_version)" (set_color green) (paradox_command_duration) (set_color normal)
 end
