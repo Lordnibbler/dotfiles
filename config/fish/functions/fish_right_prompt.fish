aa4140ce436b535462ae0f94bfe2999cb935a1b7
Ben Radler <ben@benradler.com>
2019-04-05 22:08:22 -0700

    updates for fish 3

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -2,5 +2,5 @@
 function fish_right_prompt
   set -l rb_version (command ruby -e 'print RUBY_VERSION' ^/dev/null)
   set -l node_version (command node --version ^/dev/null)
-  echo -s (set_color blue) "(💎  $rb_version  ☕️  $node_version)" (set_color green) (paradox_command_duration) (set_color normal)
+  echo -s (set_color blue) "(💎 $rb_version  ☕️ $node_version)"
 end
